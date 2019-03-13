6cdc7fbdf39fa90f0e77b99d039a84bf066459e9
Ck-Republic <Alrin12@github.com>
2019-03-13 17:41:54 +0900

    Updates

---
+++ precache-manifest.04a53c88465c820d44e38913dcd766ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30dc1f0579e72cbbbd7a22448aa35044",
+    "url": "/portfolio/static/media/wallpaper1.30dc1f05.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "1d33a1c4ccc3e5afd4e6",
+    "url": "/portfolio/static/js/main.1d33a1c4.chunk.js"
+  },
+  {
+    "revision": "292395fefae590541105",
+    "url": "/portfolio/static/js/1.292395fe.chunk.js"
+  },
+  {
+    "revision": "1d33a1c4ccc3e5afd4e6",
+    "url": "/portfolio/static/css/main.0e83251e.chunk.css"
+  },
+  {
+    "revision": "8eae7a54cf9ac4e1eb16f6aa21182577",
+    "url": "/portfolio/index.html"
+  }
+];
